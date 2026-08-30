4e506715497c742c8b643fe813aa6520b4a52a98
Your Name <you@example.com>
2026-08-30 18:25:44 +0000

    Fix invalid export syntax in dark-mode route

--- apps/example-api/src/routes/dark-mode.ts
+++ apps/example-api/src/routes/dark-mode.ts
@@ -5,4 +5,4 @@ const router = Router();
 // Adding '/' catches the stripped route perfectly
 router.use(['/', '/dark-mode', '/api/dark-mode'], darkModeRouter);
 
-export { router as 'dark-modeRouter', router as darkModeRouter, router as default };
+export { router as darkModeRouter, router as default };
